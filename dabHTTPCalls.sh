7de7026ce472ec802ff25732977ba907b85dba93
Carlos Robles <contact@croblesm.com>
2024-01-27 08:26:24 +0000

    Removing unused files, adding HTTP calls to DAB

---
+++ dabHTTPCalls.sh
@@ -0,0 +1,18 @@
+# DAB calls to HTTP endpoints
+
+# Checking DAB health
+curl -v http://localhost:5000
+
+# Checking DAB API for Books and Authors endpoints
+curl -s http://localhost:5000/api/Book | jq
+curl -s http://localhost:5000/api/Author | jq
+
+# Checking DAB API with query parameters
+curl -s http://localhost:5000/api/Book?$first=2 | jq '.value[] | {id, title}'
+curl -s http://localhost:5000/api/Author | jq '.value[1] | {id, first_name, last_name}'
+
+# Checking DAB GraphQL
+curl -X POST \
+    -H "Content-Type: application/json" \
+    -d '{"query": "{ books(first: 2, orderBy: {id: ASC}) { items { id title } } }"}' \
+    http://localhost:5000/graphql | jq
